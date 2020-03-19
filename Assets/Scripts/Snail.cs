5c5da1f63ffc328f42120ca8420e73294cba924d
TNGDev <nqcuong2@gmail.com>
2020-03-18 22:36:31 -0500

    Commit and push assets

---
+++ Assets/Scripts/Snail.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snail : MonoBehaviour
+{
+	[SerializeField] int exp = 50;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Player")
+		{
+			collision.GetComponent<PlayerController>().GainExp(exp);
+		}
+	}
+}
